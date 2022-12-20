95ad5a555a282d2af48a0dafd3c0488771bb9790
najimiman <114479872+najimiman@users.noreply.github.com>
2022-12-20 12:06:31 +0100

    factory seeder

--- prototypeBasic/database/seeders/DatabaseSeeder.php
+++ prototypeBasic/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\TachefactoryFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Tache::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
